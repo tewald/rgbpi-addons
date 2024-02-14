d42ad795b9266d65da6cc056861203fa9c8dd74f
Kevin <99993735+sd2cv@users.noreply.github.com>
2024-02-13 19:14:27 -0800

    Update RGBPI-Tweaks.sh
    shifted the screen to the left a bit to center it.

--- rgbpi-tweaks/RGBPI-Tweaks.sh
+++ rgbpi-tweaks/RGBPI-Tweaks.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+set -euo pipefail
+
+# Timings to add
+timings=(
+    "320 1 20 32 45 240 1 2 3 16 0 0 0 60.000000 0 6514560 1"
+    "720 1 46 69 100 480 1 3 6 34 0 0 0 30 1 14670150 1"
+    "720 1 29 69 117 576 1 7 6 38 0 0 0 25 1 14656125 1"
+)
+
+file="/opt/rgbpi/ui/data/timings.dat"
+
+# Check if the timings already exist in the file
+for line in "${timings[@]}"; do
+    if ! grep -qF "$line" "$file"; then
+        echo "$line" >> "$file"
+    fi
+done
 
 set -euo pipefail
 
 cd "$(dirname "$(readlink -f "$0")")"
+
 
 python main.py
