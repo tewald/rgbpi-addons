0eca9fb53a0694ddc54e87fc68c4558b49b6904c
Kevin <99993735+sd2cv@users.noreply.github.com>
2024-03-20 16:47:27 -0700

    Update main.py

--- RGBPi-Extra/application/main.py
+++ RGBPi-Extra/application/main.py
@@ -42,7 +42,7 @@ menu_theme.scrollbar_slider_color = (0, 0, 0, 255)
 menu_theme.scrollbar_thick = (5)
 
 menu = pygame_menu.Menu(
-    title='RGB-Pi OS4 Extras',
+    title='RGBPi OS4 Extra',
     theme=menu_theme,
     joystick_enabled=True,
     width=WINDOW_SIZE[0],
